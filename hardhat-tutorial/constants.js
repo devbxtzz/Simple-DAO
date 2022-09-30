421a7bcac7753cf9f8320e111b95f3b1e0f543e7
devbxtzz <batztonnie@gmail.com>
2022-09-30 10:33:02 +0300

    Smart contract code

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x52B573aacBbd89611DD0C66d7A954388A4Ec193b";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
